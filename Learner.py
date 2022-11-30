39e741cc8607c9117fbc1f8ff006f0462af616b9
Nima Yazdi <nima.yazdi93@gmail.com>
2022-11-30 17:00:27 +0330

    Final

--- Learner.py
+++ Learner.py
@@ -1,48 +1,35 @@
 import os 
 from SSA import SSA_H_Plus
 from FeatureExtractor import FeatureExtractor 
-import scipy.io as sio  
-from matplotlib.pylab import plot
+import scipy.io as sio
+
 fe=FeatureExtractor() 
-image_paths = [] 
-dataset='dogs' 
-image_folder = os.getcwd()+'/data-set/cars/cars_train/'
-print(image_folder)
+image_paths = []  
+image_folder = os.getcwd()+'/data-set/cars/cars_train'
+
 for path, subdirs, files in os.walk(image_folder):
     for name in files:
         if(name.endswith('.jpg')):
             image_paths.append(os.path.join(path, name)) 
 
 imgPath_feature=[]
-imgPath_obj=[]
-k=1
+imgPath_obj=[] 
 feature_matrix=[]
+k=0
 for img_path in image_paths:
-    feature=fe.extract(img_path) 
-    # if k==2000:
-    #     break
-    # if k==1:
-    #     feature_matrix=feature
-       
-    # else:
-    #     feature_matrix=np.vstack([feature_matrix,feature])
-    k=k+1
+    feature=fe.extract(img_path)
     feature_matrix.append(feature)
-    imgPath_obj.append(img_path) 
-
+    imgPath_obj.append(img_path)
+    k=k+1
+    print('VGG:'+str(k))
 imgPath_obj 
-sio.savemat('cached-data/new/cars/img_path_obj_cars.mat',{"img_path_obj":imgPath_obj})
- 
-k=1
+sio.savemat('cached-data/new/cars/img_path_cars.mat',{"img_path_obj":imgPath_obj})
+
+k=0
 ssa_plus_features=[]
 for fet in feature_matrix:
-    ssa_plus=SSA_H_Plus(fet)  
-    # if k==1:
-    #     ssa_plus_features=ssa_plus 
-    #     k=0
-    # else:
-    #     ssa_plus_features=np.vstack([ssa_plus_features,ssa_plus]) 
+    ssa_plus=SSA_H_Plus(fet)   
     ssa_plus_features.append(ssa_plus)
+    k=k+1
+    print('SSA:'+str(k))
 sio.savemat('cached-data/new/cars/ssa_features_cars.mat',{'ssa_features':ssa_plus_features})
-
-
